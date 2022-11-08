f6dfbb3ec18289720dadee7f86182a872f9ab612
Tom Draper <tomjdraper1@gmail.com>
2022-11-08 12:44:56 +0000

    Syntax highlighting

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,16 +1,41 @@
 <script lang="ts">
-  let hi: string = "hi"
+  import {onMount} from 'svelte';
+  import hljs from 'highlight.js';
+  import 'highlight.js/styles/atom-one-dark.css';
+
+  // hljs.highlightAll();
+
+  function renderCode() {
+    let snippet = document.getElementById("code");
+    if (snippet != null) {
+      snippet.innerHTML = code
+      hljs.highlightElement(snippet);
+    }
+    console.log(code)
+    console.log(snippet)
+  }
+
   let borderRadius = 10;
   let terminalBackground = '#25323a'
   let background = '#bfe4f7'
   let fontSize = 16;
+  let padding = 15;
+  let code = "from dataclass import dataclass\nimport numpy as np"
+  $: code && renderCode();
+  $: code == "" && renderCode()
+  let language = "python"
+  let font = "Fira Code"
+  onMount(() => {
+    renderCode()
+  })
 </script>
 
 <div>
   <div class="page-content">
     <div class="viewer" style="background: {background};">
       <div class="code-snippet-container" style="border-radius: {borderRadius}px;">
-        <textarea  id="codeSnippet" name="code-snippet" rows="30" cols="80" style="background: {terminalBackground}; border-radius: {borderRadius}px; font-size: {fontSize}px;"/>
+        <pre><code id="code" class="language-{language}" style="font-family: {font} !important; margin: {padding}px">{code}</code></pre>
+        <textarea bind:value={code} id="codeSnippet" name="code-snippet" style="background: {terminalBackground}; border-radius: {borderRadius}px; font-size: {fontSize}px; font-family: {font} !important; padding: {padding}px;"/>
       </div>
     </div>
     <div class="controller">Controller</div>
@@ -34,12 +59,32 @@
     width: 350px;
   }
   #codeSnippet {
-    color: white;
-    width: 100%;
-    height: 100%;
+    color: transparent;
     background: rgb(176, 176, 176);
-    padding: 20px;
     border: none;
     resize: none;
+    caret-color: white;
+    width: 710px;
+    height: 500px;
   }
+
+  :global(pre) {
+    position: absolute !important;
+  }
+
+  pre {
+    pointer-events: none;
+    margin: 0;
+  }
+  pre code {
+    padding: 0;
+    width: 700px;
+  }
+
+  #code {
+    background: transparent !important;
+    width: 700px;
+    white-space: pre-wrap;
+  }
+
 </style>
